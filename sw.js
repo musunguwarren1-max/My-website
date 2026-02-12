74f9397fe4a85c580bff1d5ab211a4bf59e10420
root <root@localhost.localdomain>
2026-02-12 10:13:19 +0000

    Advanced full website with blog and AdSense

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-self.addEventListener("install", e=>{
-  self.skipWaiting();
-});
-self.addEventListener("fetch", e=>{
+const cards=document.querySelectorAll(".card");
+window.addEventListener("scroll",()=>{
+  cards.forEach(card=>{
+    const pos=card.getBoundingClientRect().top;
+    if(pos<window.innerHeight-50){
+      card.style.opacity=1;
+      card.style.transform="translateY(0)";
+    }
+  });
 });
